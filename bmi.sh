31ec262886d98ca937764cf07a0d4be7c40858cd
fusion809 <brentonhorne77@gmail.com>
2018-02-13 00:17:28 +1000

    Adding bmiim func and moving height.sh to 03-height.sh

---
+++ bmi.sh
@@ -0,0 +1,8 @@
+function bmiim {
+    # BMI from imperial measurements
+    # $1 is weight in lb
+    # $2 is height in ft, rounding down
+    # $3 is the remainder height in inches
+    kg=$(octe "0.4535924*$1" | sed 's/ans =\s*//g')
+    cm=$(fitocm $2 $3)
+}
