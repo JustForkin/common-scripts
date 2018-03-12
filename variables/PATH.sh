ffd414633fb86a1ed2324cdcb1dd0be06384ef99
fusion809 <brentonhorne77@gmail.com>
2018-03-12 18:15:12 +1000

    Updating variables

--- variables/PATH.sh
+++ variables/PATH.sh
@@ -1,18 +1,63 @@
+# Nix
+if [[ -d $HOME/.nix-profile/share ]]; then
+    export XDG_DATA_DIRS=$HOME/.nix-profile/share
+fi
+
+# Rubygems
 export GEM_VERSION=$(ruby --version | cut -d ' ' -f 2 | sed 's/[a-z][0-9]//g')
-unset PATH
 export GEMPATH=$HOME/.gem/ruby/$GEM_VERSION/bin
-export GOROOT=/usr/lib/go
-export GOPATH=$HOME/go
-export JVM_HOME=/usr/lib/jvm
-export JAVA_HOME=$JVM_HOME/java-8-openjdk
-export PERL_PATH=/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
-export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin:/usr/lib/hardening-wrapper/bin
+
+# Go
+unset GOROOT
+unset GOPATH
+if [[ -d /usr/lib/go ]]; then
+    export GOROOT=/usr/lib/go
+
+    if [[ -d $HOME/go ]]; then
+         export GOPATH=$HOME/go
+    fi
+fi
+
+# Java
+unset JAVA_HOME
+unset JVM_HOME
+if [[ -d /usr/lib/jvm ]]; then
+    export JVM_HOME=/usr/lib/jvm
+    if [[ -d $JVM_HOME/java-8-openjdk ]]; then
+         export JAVA_HOME=$JVM_HOME/java-8-openjdk
+    elif [[ -d $JVM_HOME/java-9-openjdk ]]; then
+         export JAVA_HOME=$JVM_HOME/java-9-openjdk
+    elif [[ -d $JVM_HOME/java-9-openjdk ]]; then
+         export JAVA_HOME=$JVM_HOME/java-7-openjdk
+    fi
+fi
+
+# Perl
+unset PERL_PATH
+if [[ -d /usr/bin/site_perl ]]; then
+    export PERL_PATH=$PERL_PATH:/usr/bin/site_perl
+fi
+if [[ -d /usr/bin/vendor_perl ]]; then
+    export PERL_PATH=$PERL_PATH:/usr/bin/vendor_perl
+fi
+if [[ -d /usr/bin/core_perl ]]; then
+    export PERL_PATH=$PERL_PATH:/usr/bin/core_perl
+fi
+
+# PATH
+export PATH=
+if [[ -d /usr/lib/hardening-wrapper/bin ]]; then
+    export PATH=$PATH:/usr/lib/hardening-wrapper/bin
+fi
+
+export PATH=/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin
 export PATH=$PATH:$JAVA_HOME/bin
 export PATH=$PATH:$GEMPATH
 export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
 export PATH=$PATH:$PERL_PATH
 export PATH=$PATH:$HOME/.nix-profile/bin
-if [[ -d $HOME/.gem/ruby/2.3.0/bin ]]; then
-    export PATH=$PATH:$HOME/.gem/ruby/2.3.0/bin
+export PATH=$PATH:$GEMPATH
+
+if [[ -d $HMOE/.nix-profile/bin ]]; then
+    export PATH=$PATH:$HOME/.nix-profile/bin
 fi
-export XDG_DATA_DIRS=$HOME/.nix-profile/share
