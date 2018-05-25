15615d2521131304b12ccebd5799bf2c70cc3a65
fusion809 <brentonhorne77@gmail.com>
2018-05-25 14:08:19 +1000

    Moving octcli and octe commands from debian scripts into this folder

--- 02-octave.sh
+++ 02-octave.sh
@@ -1,10 +1,30 @@
+if ! [[ -f /usr/bin/octave ]]; then 
+    if ! [[ -f $HOME/.nix-profile/bin/octave ]]; then
+         if [[ -f /usr/bin/flatpak ]]; then
+              function octave {
+    flatpak run org.octave.Octave
+}
+         fi
+    fi
+fi
+ 
 function octcli {
-    octave --no-gui
+    if ! `which octave-cli`; then
+         if `which octave`; then
+              octave --no-gui "$@"
+         fi
+    else
+         octave-cli "$@"
+    fi
 }
 
 function octe {
-    octave --eval $@
-}
+    if  [[ -f /usr/bin/octave ]]; then
+         octave --eval "$1"
+    elif [[ -f $HOME/.nix-profile/bin/octave-cli ]]; then
+         octave-cli --eval "$1"
+    fi
+} 
 
 for i in $HOME/Shell/common-scripts/octave/*.sh
 do
