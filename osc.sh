aac4527a93b5ca1f90f36c4cc41cf29d781c8863
fusion809 <brentonhorne77@gmail.com>
2018-06-03 03:08:31 +1000

    cdgm->cdpk OpenRA in openrabup

--- osc.sh
+++ osc.sh
@@ -70,7 +70,7 @@ function openrabup {
          printf "OpenRA Bleed is up to date!\n"
     else
          printf "Updating my folk of the OpenRA repository.\n"
-         cdgm OpenRA ; git checkout bleed -q ; git pull upstream bleed -q ; git push origin bleed -q
+         cdpk OpenRA ; git checkout bleed -q ; git pull upstream bleed -q ; git push origin bleed -q
          printf "Updating OBS repo openra-bleed.\n"
          sed -i -e "s/$specn/$mastn/g" $OBSH/openra-bleed/openra-bleed.spec
          sed -i -e "s/$specm/$comm/g" $OBSH/openra-bleed/openra-bleed.spec
