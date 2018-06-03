c887458318272db41226b99bd3761582721c1a60
fusion809 <brentonhorne77@gmail.com>
2018-06-03 19:46:01 +1000

    Fixing cd/home file paths as listed in 01-home.sh

--- cd/01-home.sh
+++ cd/01-home.sh
@@ -5,14 +5,27 @@ function cda {
 # Chem
 . $(dirname "$0")/home/chem.sh
 
-# 
+# Docs
+. $(dirname "$0")/home/documents.sh
+
+# Downloads
 function cddo {
     cd $HOME/Downloads/$1
 }
 
+# Music
 function cdm {
     cd $HOME/Music/$1
 }
+
+# OBS
+. $(dirname "$0")/home/obs.sh
+
+# Pictures
+. $(dirname "$0")/home/pictures.sh
+
+# Programs
+. $(dirname "$0")/home/programs.sh
 
 # cd to Shell
 function cdsh {
@@ -23,3 +36,9 @@ function cdsh {
 function cdt {
     cd "$HOME/Textbooks/$1"
 }
+
+# Videos
+. $(dirname "$0")/home/videos.sh
+
+# VirtualBox VMs
+. $(dirname "$0")/home/virtualbox.sh
