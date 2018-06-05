0b43b513637c509a8af1c207be7a3dc952865ee9
fusion809 <brentonhorne77@gmail.com>
2018-06-05 13:29:37 +1000

    Adding apm aliases

--- apm.sh
+++ apm.sh
@@ -2,13 +2,19 @@ function apmu {
     apm upgrade --no-confirm
 }
 
+alias apmup=apmu
+
 function apmi {
     apm install "$@"
 }
 
+alias apmin=apmi
+
 function apmr {
     apm remove "$@"
 }
+
+alias apmrm=apmr
 
 function apms {
     apm search "$@"
