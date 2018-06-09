46fa5dd16c57038b8258b50d83f1f68eeeea0237
fusion809 <brentonhorne77@gmail.com>
2018-06-09 14:46:27 +1000

    Adding babelup

--- osc.sh
+++ osc.sh
@@ -98,6 +98,26 @@ function ra2up {
     fi
 }
 
+function babelup {
+    cdgo openbabel
+    git pull origin master -q
+    mastn=$(git rev-list --branches master --count)
+    specn=$(cat $OBS/home:fusion809:openbabel3/openbabel3/openbabel3.spec | grep "Version:" | sed 's/Version:\s*//g')
+    comm=$(git log | head -n 1 | cut -d ' ' -f 2)
+    specm=$(cat $OBS/home:fusion809:openbabel3/openbabel3/openbabel3.spec | grep "define commit" | cut -d ' ' -f 3)
+
+    if [[ $specn == $mastn ]]; then
+         printf "Open Babel is up to date!\n"
+    else
+         sed -i -e "s/$specn/$mastn/g" $OBS/home:fusion809:openbabel3/openbabel3/openbabel3.spec
+         sed -i -e "s/$specm/$comm/g" $OBS/home:fusion809:openbabel3/openbabel3/openbabel3.spec
+         cdobs home:fusion809:openbabel3/openbabel3
+         osc ci -m "Bumping $specn->$mastn"
+    fi
+}
+
+alias openbabelup=babelup
+
 function gsup {
     pkgver=$(wget -cqO- https://github.com/GNOME/gnome-software/releases | grep "tar\.gz" | grep "3\.[02468]*\.[0-9]*" | cut -d '/' -f 5 | cut -d '"' -f 1 | sed 's/\.tar\.gz//g' | head -n 1)
     pkgpver=$(cat $OBSH/gnome-software/gnome-software.spec | grep "Version:" | cut -d ':' -f 2 | sed 's/\s*//g')
