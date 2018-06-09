8c98e24eccb7bb1f4c9b2c4d0191bb9ae81b8627
fusion809 <brentonhorne77@gmail.com>
2018-06-09 20:44:24 +1000

    Adding warning about Flatpaks and opendesktop-app's push func

--- 02-octave.sh
+++ 02-octave.sh
@@ -1,6 +1,7 @@
 if ! [[ -f /usr/bin/octave ]]; then 
     if ! [[ -f $HOME/.nix-profile/bin/octave ]]; then
          if [[ -f /usr/bin/flatpak ]]; then
+              printf "Warning, the push function for the opendesktop-app repository will NOT work with Flatpaks...\n"
               function octave {
     flatpak run org.octave.Octave
 }
@@ -20,9 +21,9 @@ function octcli {
 
 function octe {
     if  [[ -f /usr/bin/octave ]]; then
-         octave --eval "$1"
+         octave --eval "$1" | sed "s/ans =\s*//g"
     elif [[ -f $HOME/.nix-profile/bin/octave-cli ]]; then
-         octave-cli --eval "$1"
+         octave-cli --eval "$1" | sed "s/ans =\s*//g"
     fi
 } 
 
