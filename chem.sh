47902fa860fea5d8ffa641f673cbdb07dd2070f0
fusion809 <brentonhorne77@gmail.com>
2018-06-19 03:07:02 +1000

    Rm optipng from charcp func

--- chem.sh
+++ chem.sh
@@ -1,10 +1,3 @@
-function opticp {
-    if `echo $1 | grep png`; then
-         optipng -o7 "$1"
-    fi
-    cp "$1" .
-}
-
 function charcp {
-    find /data/Chem -name "*$(echo $PWD | cut -d '-' -f 2)*" -exec opticp '{}' \;
+    find /data/Chem -name "*$(echo $PWD | cut -d '-' -f 2)*" -exec cp '{}' . \;
 }
