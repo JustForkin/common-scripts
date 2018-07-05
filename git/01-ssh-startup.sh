4c4b1d68b8417817e02228aa33196dd29d7a9509
fusion809 <brentonhorne77@gmail.com>
2018-07-06 06:42:56 +1000

    Rm gitconfig.sh script, no longer needed

--- git/01-ssh-startup.sh
+++ git/01-ssh-startup.sh
@@ -4,10 +4,6 @@
 #############################################################
 # Sign in with SSH at startup
 # Makes contributing to GitHub projects a lot simpler.
-if [[ -f $HOME/.gitconfig.sh ]]; then
-	source $HOME/.gitconfig.sh
-fi
-
 if [[ -a $HOME/.ssh/environment ]]; then
 	SSH_ENV=$HOME/.ssh/environment
 elif [[ $USER == fusion809 ]] && ! [[ -f $HOME/.ssh/id_rsa.pub ]]; then
