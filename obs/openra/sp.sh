cccb04840d42b4ef9e8acbdc619aa1b9b6ed81a7
Brenton Horne <brentonhorne77@gmail.com>
2018-07-31 19:17:04 +1000

    Adding some comments to spup

--- obs/openra/sp.sh
+++ obs/openra/sp.sh
@@ -9,6 +9,7 @@ function spup {
     sdkpver=$(cat /data/OBS/home:fusion809/openra-sp/openra-sp.spec | grep "commit" | sed 's/%define commit //g')
     sdkpc=$(cat /data/OBS/home:fusion809/openra-sp/openra-sp.spec | grep "Version:" | sed 's/Version:\s*//g')
 
+    # If OpenRAModSDK is outdated sed update it
     if [[ $sdklc == $sdkpc ]]; then
          printf "SDK is up-to-date!\n"
     else
@@ -17,6 +18,7 @@ function spup {
          sed -i -e "s|$sdkpver|$sdklver|g" openra-sp.spec
     fi
 
+    # If Shattered Paradise is outdated sed update it
     if [[ $splver == $sppver ]]; then
          printf "Latest Shattered Paradise commit is packaged!\n"
     else
@@ -24,6 +26,7 @@ function spup {
          sed -i -e "s|$sppver|$splver|g" openra-sp.spec
     fi
 
+    # If OpenRAModSDK or Shattered Paradise repo is outdated commit updtae to repo
     if ! [[ $sdkpver == $sdkpver ]] || ! [[ $splver == $sppver ]]; then
          cdobsh openra-sp
          osc ci -m "Bumping to SDK: $sdklc ($sdklver); SP: $splver"
