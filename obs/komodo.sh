91f22d27fd94545b5b826e2e5048c4c2d197f0a4
Brenton Horne <brentonhorne77@gmail.com>
2018-07-31 16:52:08 +1000

    Adding komodoup func

---
+++ obs/komodo.sh
@@ -0,0 +1,11 @@
+function komodoup {
+    cdobsh komodo-edit
+    pkglver=$(curl -sL https://www.activestate.com/komodo-ide/downloads/edit | grep "linux-x86_64" | cut -d '/' -f 10)
+    pkgpver=$(komodo-edit.spec | grep "Version:" | cut -d ':' -f 2 | sed 's/\s*//g'))
+    sed -i -e "s|$pkgpver|$pkglver|g" komodo-edit.spec
+    osc ci -m "Bumping $pkglver"
+}
+
+alias kup=komodoup
+alias kedup=komodoup
+alias komodoeditup=komodoup
