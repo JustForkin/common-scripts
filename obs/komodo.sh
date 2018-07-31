3acf16c0252eaa19859ce38509dc0f6a232b449e
Brenton Horne <brentonhorne77@gmail.com>
2018-07-31 17:17:38 +1000

    Rm errant ) from komup

--- obs/komodo.sh
+++ obs/komodo.sh
@@ -1,7 +1,7 @@
 function komodoup {
     cdobsh komodo-edit
     pkglver=$(curl -sL https://www.activestate.com/komodo-ide/downloads/edit | grep "linux-x86_64" | cut -d '/' -f 10)
-    pkgpver=$(komodo-edit.spec | grep "Version:" | cut -d ':' -f 2 | sed 's/\s*//g'))
+    pkgpver=$(komodo-edit.spec | grep "Version:" | cut -d ':' -f 2 | sed 's/\s*//g')
     sed -i -e "s|$pkgpver|$pkglver|g" komodo-edit.spec
     osc ci -m "Bumping $pkglver"
 }
