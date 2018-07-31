8da2b002d409a956e0a3b8657fd70f6ad1dd9316
Brenton Horne <brentonhorne77@gmail.com>
2018-07-31 23:51:14 +1000

    Fixing scom to eliminate ShellCheck warnings

--- git/08-get.sh
+++ git/08-get.sh
@@ -1,11 +1,13 @@
+# Get latest git commit from repo data
 function lcom {
     git log | head -n 1 | cut -d ' ' -f 2 | xclip -sel clip
 }
 
+# Get latest packaged commit from spec file
 function scom {
-    if `ls * | grep "\.spec" > /dev/null 2>&1`; then
-         cat $(ls * | grep "\.spec" | head -n 1) | grep "define commit" | cut -d ' ' -f 3 | xclip -sel clip
+    if ls -ld ./*.spec > /dev/null 2>&1; then
+         grep "define commit" < "$(find . -name "*.spec" | grep -v ".osc" | head -n 1)" | cut -d ' ' -f 3 | xclip -sel clip
     elif [[ -n $1 ]]; then
-         cat $1 | grep "define commit" | cut -d ' ' -f 3 | xclip -sel clip
+        grep "define commit" < "$1" | cut -d ' ' -f 3 | xclip -sel clip
     fi
 }
