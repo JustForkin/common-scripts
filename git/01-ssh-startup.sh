323c00b9a7a470f2fc2e99e86f30532449c419c4
Brenton Horne <brentonhorne77@gmail.com>
2018-08-03 18:59:52 +1000

    Fixing SSH authentication issue

--- git/01-ssh-startup.sh
+++ git/01-ssh-startup.sh
@@ -4,39 +4,43 @@
 #############################################################
 # Sign in with SSH at startup
 # Makes contributing to GitHub projects a lot simpler.
-if [[ -a "$HOME/.ssh/environment" ]]; then
-	SSH_ENV="$HOME/.ssh/environment"
-elif [[ "$USER" == fusion809 ]] && ! [[ -f "$HOME/.ssh/id_rsa.pub" ]]; then
+if grep -i Void < /etc/os-release > /dev/null 2>&1 ; then
+    eval `keychain -q --eval aur` &> /tmp/aur.log
+    eval `keychain -q --eval id_rsa` &> /tmp/id_rsa.log
+else
+    if [[ -a "$HOME/.ssh/environment" ]]; then
+       	SSH_ENV="$HOME/.ssh/environment"
+    elif [[ "$USER" == fusion809 ]] && ! [[ -f "$HOME/.ssh/id_rsa.pub" ]]; then
 	ssh-keygen -t rsa -b 4096 -C "brentonhorne77@gmail.com"
 	SSH_ENV="$HOME/.ssh/environment"
 	git config --global user.name "fusion809"
 	git config --global user.email "brentonhorne77@gmail.com"
-elif [[ -n $EMAIL ]] && ! [[ -f "$HOME/.ssh/id_rsa.pub" ]]; then
+    elif [[ -n $EMAIL ]] && ! [[ -f "$HOME/.ssh/id_rsa.pub" ]]; then
 	ssh-keygen -t rsa -b 4096 -C "$EMAIL"
 	SSH_ENV="$HOME/.ssh/environment"
 	git config --global user.name "$USER"
 	git config --global user.email "$EMAIL"
-fi
-
-if ! which keychain > /dev/null 2>&1; then
-    if grep -i openSUSE < /etc/os-release > /dev/null 2>&1; then
-         sudo zypper in -y keychain
-    elif grep -i Fedora < /etc/os-release > /dev/null 2>&1; then
-         sudo dnf install -y keychain
-    elif grep -i "Debian\|Ubuntu\|deepin" < /etc/os-release > /dev/null 2>&1; then
-         sudo apt install -y keychain
-    elif grep -i "CentOS" < /etc/os-release > /dev/null 2>&1; then
-         sudo yum install -y keychain
-    elif grep -i "Arch" < /etc/os-release > /dev/null 2>&1; then
-         sudo pacman -S keychain --noconfirm
-    elif grep -i "Gentoo" < /etc/os-release > /dev/null 2>&1; then
-         sudo emerge net-misc/keychain
-    elif grep -i "Void" < /etc/os-release > /dev/null 2>&1; then
-         sudo xbps-install -y keychain
-    elif grep -i "NixOS" < /etc/os-release > /dev/null 2>&1; then
-         nix-env -i keychain
     fi
-fi
+
+    if ! which keychain > /dev/null 2>&1; then
+         if grep -i openSUSE < /etc/os-release > /dev/null 2>&1; then
+              sudo zypper in -y keychain
+         elif grep -i Fedora < /etc/os-release > /dev/null 2>&1; then
+              sudo dnf install -y keychain
+         elif grep -i "Debian\|Ubuntu\|deepin" < /etc/os-release > /dev/null 2>&1; then
+              sudo apt install -y keychain
+         elif grep -i "CentOS" < /etc/os-release > /dev/null 2>&1; then
+              sudo yum install -y keychain
+         elif grep -i "Arch" < /etc/os-release > /dev/null 2>&1; then
+              sudo pacman -S keychain --noconfirm
+         elif grep -i "Gentoo" < /etc/os-release > /dev/null 2>&1; then
+              sudo emerge net-misc/keychain
+         elif grep -i "Void" < /etc/os-release > /dev/null 2>&1; then
+              sudo xbps-install -y keychain
+         elif grep -i "NixOS" < /etc/os-release > /dev/null 2>&1; then
+              nix-env -i keychain
+         fi
+    fi
 
         if ! grep Mageia > /dev/null 2>&1; then
 	      eval keychain -q --eval id_rsa
@@ -44,7 +48,7 @@ fi
 
 
 # AUR
-if [[ $USER == "fusion809" ]] || [[ $AUR == "true" ]]; then
+    if [[ $USER == "fusion809" ]] || [[ $AUR == "true" ]]; then
 	if ! [[ -f "$HOME"/.ssh/config ]]; then
 		echo "Host aur.archlinux.org\n  IdentityFile ~/.ssh/aur\n  User aur" > "$HOME"/.ssh/config
 	fi
@@ -59,8 +63,8 @@ if [[ $USER == "fusion809" ]] || [[ $AUR == "true" ]]; then
         if ! grep Mageia > /dev/null 2>&1; then
 	      eval keychain -q --eval aur
         fi
-else
+    else
 	printf "%s\n" "Add an AUR=true line to your $HOME/.gitconfig.sh file in order to set up SSH to authenticate AUR commits."
+    fi
 fi
-
 #############################################################
