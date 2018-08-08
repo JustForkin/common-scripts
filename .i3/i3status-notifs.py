29748d0ed26c851941b5bba85a8f653745e2f2a8
Brenton Horne <brentonhorne77@gmail.com>
2018-08-08 21:18:08 +1000

    Rm Juci++ checks and renaming makepkg script

--- .i3/i3status-notifs.py
+++ .i3/i3status-notifs.py
@@ -153,11 +153,6 @@ status.register("shell",
     interval=300)
 
 status.register("shell",
-    command="/usr/local/bin/jucippn",
-    color="#F9B999",
-    interval=300)
-
-status.register("shell",
     command="/usr/local/bin/jmoln",
     color="#A2F412",
     interval=300)
