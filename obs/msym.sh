3c5e3a8d2a608d23bb94be238aa6e18dda9e62c8
Brenton Horne <brentonhorne77@gmail.com>
2018-08-12 22:56:45 +1000

    libmsym.spec->libmsym0_2.spec

--- obs/msym.sh
+++ obs/msym.sh
@@ -4,14 +4,14 @@ function msymup {
     mastn=$(comno)
     comm=$(git log | head -n 1 | cut -d ' ' -f 2)
     cdobsh libmsym
-    specn=$(cat libmsym.spec | grep "Version:" | sed 's/Version:\s*//g')
-    specm=$(cat libmsym.spec | grep "define commit" | cut -d ' ' -f 3)
+    specn=$(cat libmsym0_2.spec | grep "Version:" | sed 's/Version:\s*//g')
+    specm=$(cat libmsym0_2.spec | grep "define commit" | cut -d ' ' -f 3)
 
     if [[ $specn == $mastn ]]; then
          printf "libmsym is up to date!\n"
     else
          sed -i -e "s/$specn/$mastn/g" \
-                -e "s/$specm/$comm/g" libmsym.spec
+                -e "s/$specm/$comm/g" libmsym0_2.spec
          osc ci -m "Bumping $specn->$mastn"
     fi
 }
