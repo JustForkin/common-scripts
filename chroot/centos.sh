7af2b5f93240afada6c97803683148ee80af39d9
Brenton Horne <brentonhorne77@gmail.com>
2018-08-23 19:02:40 +1000

    Fixing croot func (adding boot mount)

--- chroot/centos.sh
+++ chroot/centos.sh
@@ -1,3 +1,10 @@
 function croot {
+	BOOT_PART=$(ls -ld /dev/disk/by-label/* | grep CentOS_Boot | cut -d ' ' -f 11 | cut -d '/' -f 3)
+	PART=$(ls -ld /dev/mapper/* | cut -d '/' -f 4 | cut -d ' ' -f 1 | grep -i centos)
+	if ! [[ -d /centos ]]; then
+		mkdir -p /centos
+	fi
+	sudo mount /dev/mapper/$PART /centos
+	sudo mount /dev/$BOOT_PART /centos/boot
 	genroot /centos
 }
