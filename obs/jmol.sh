57951fd50eaf11127b10ca10c20d4d72c9a5e802
Brenton Horne <brentonhorne77@gmail.com>
2018-08-25 17:28:02 +1000

    Fixing jmolup, so it also seds _service now and uses a new method to determine latest version

--- obs/jmol.sh
+++ obs/jmol.sh
@@ -3,7 +3,7 @@ if `which curl > /dev/null 2>&1`; then
     MAJOR=$(curl -sL https://sourceforge.net/projects/jmol/files/Jmol/ | grep "Version%20[0-9]" | head -n 1 | cut -d '/' -f 6 | sed 's/Version%20//g')
     pkgver=$(wget -cqO- "https://sourceforge.net/projects/jmol/files/Jmol/Version%20${MAJOR}" | grep "Jmol%20${MAJOR}" | head -n 1 | cut -d '/' -f 7 | sed 's/Jmol%20//g')
 elif `which wget > /dev/null 2>&1`; then
-    MAJOR=$(wget -cqO- https://sourceforge.net/projects/jmol/files/Jmol/ | | grep "Version%20[0-9]" | head -n 1 | cut -d '/' -f 6 | sed 's/Version%20//g')
+    MAJOR=$(wget -cqO- https://sourceforge.net/projects/jmol/files/Jmol/ | grep "Version%20[0-9]" | head -n 1 | cut -d '/' -f 6 | sed 's/Version%20//g')
     pkgver=$(wget -cqO- "https://sourceforge.net/projects/jmol/files/Jmol/Version%20${MAJOR}" | grep "Jmol%20${MAJOR}" | head -  n 1 | cut -d '/' -f 7 | sed 's/Jmol%20//g')
 else
     printf "Neither cURL nor wget have been detected, please install them or, if already installed, add them to the system PATH.\n"
