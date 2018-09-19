940b5e7ca3aee51a8c8523b75ad414dd64ec4c74
Brenton Horne <brentonhorne77@gmail.com>
2018-09-19 18:07:42 +0000

    Making groot mount up sdb1

--- chroot/gentoo.sh
+++ chroot/gentoo.sh
@@ -2,6 +2,9 @@ function groot {
     if ! [[ -f /gentoo/bin/bash ]]; then
          sudo mount /dev/sda5 /gentoo
     fi
+	if ! [[ -f /gentoo/data/Documents ]]; then
+	sudo mount /dev/sdb1 /gentoo/data
+	fi
 
-    genroot /gentoo
+    genbasic /gentoo
 }
