e7b193ce973830d632252fa2241f747c064baf73
Brenton Horne <brentonhorne77@gmail.com>
2018-09-25 16:10:07 +1000

    Improving on aroot

--- chroot/arch.sh
+++ chroot/arch.sh
@@ -1,3 +1,9 @@
 function aroot {
-    genbasic /arch
+	if ! [[ -d /arch/bin ]]; then
+		sudo mount /dev/sda6 /arch
+	fi
+	if ! [[ -d /arch/data/Documents ]]; then
+		sudo mount /dev/sdb1 /arch/data
+	fi
+    	genbasic /arch
 }
