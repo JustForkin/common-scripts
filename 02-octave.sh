ebdaf3d567b586f8be96e2955a5bfc044d79e51a
Brenton Horne <brentonhorne77@gmail.com>
2018-09-28 22:06:33 +1000

    Making 02-octave more ShellCheck-friendly

--- 02-octave.sh
+++ 02-octave.sh
@@ -1,17 +1,17 @@
-if ! [[ -f /usr/bin/octave ]]; then 
-    if ! [[ -f $HOME/.nix-profile/bin/octave ]]; then
-         if [[ -f /usr/bin/flatpak ]]; then
-              printf "Warning, the push function for the opendesktop-app repository will NOT work with Flatpaks...\n"
-              function octave {
+if ! command -v octave > /dev/null 2>&1; then
+	if [[ -f /usr/bin/flatpak ]] && flatpak list | grep -i octave; then
+		printf "Warning, the push function for the opendesktop-app repository will NOT work with Flatpaks...\n"
+		function octave {
     flatpak run org.octave.Octave
 }
-         fi
-    fi
+    else
+        printf "%s\n" "You may wish to install GNU Octave mate, as several functions defined in $HOME/Shell/common-scripts will not work without it."
+	fi
 fi
  
 function octcli {
-    if ! `which octave-cli`; then
-         if `which octave`; then
+    if ! command -v octave-cli > /dev/null 2>&1; then
+         if command -v octave > /dev/null 2>&1; then
               octave --no-gui "$@"
          fi
     else
@@ -20,7 +20,7 @@ function octcli {
 }
 
 function octe {
-    if  [[ -f /usr/bin/octave ]]; then
+    if  command -v octave; then
          octave --eval "$1" | sed "s/ans =\s*//g"
     elif [[ -f $HOME/.nix-profile/bin/octave-cli ]]; then
          octave-cli --eval "$1" | sed "s/ans =\s*//g"
