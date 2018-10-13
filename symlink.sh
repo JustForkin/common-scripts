98a05ad3051a17381d8e21b4745452616467cc07
Brenton Horne <brentonhorne77@gmail.com>
2018-10-13 15:40:26 +0000

    Adding code to symlink.sh to automatically set up shell symlinks properly

--- symlink.sh
+++ symlink.sh
@@ -5,3 +5,11 @@ function symlink {
 		sudo ln -sf $SCR/common-scripts/usr/local/bin/* /usr/local/bin/
 	fi
 }
+
+export ID=$(cat /etc/os-release | grep "^ID=" | cut -d "=" -f 2)
+
+if ! ls -ld $HOME/Shell | grep -i "$ID" > /dev/null 2>&1 ; then
+	printf "%s$ID\n" '$ID is '
+	ln -sf $SCR/$ID-scripts/{.bashrc,.zshrc,Shell} $HOME/
+	sudo ln -sf $SCR/$ID-scripts/root/{.bashrc,.zshrc,Shell} /root/
+fi
