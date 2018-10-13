d9e7325ea621ad10e9b835d6e2530fae0a6f8f32
Brenton Horne <brentonhorne77@gmail.com>
2018-10-13 15:00:37 +0000

    Improving on symlink func

--- symlink.sh
+++ symlink.sh
@@ -1,3 +1,7 @@
 function symlink {
-	sudo ln -sf $PWD/"$@" /usr/local/bin/
+	if [[ -n "$1" ]]; then	
+		sudo ln -sf $SCR/common-scripts/usr/local/bin/"$@" /usr/local/bin/
+	else
+		sudo ln -sf $SCR/common-scripts/usr/local/bin/* /usr/local/bin/
+	fi
 }
