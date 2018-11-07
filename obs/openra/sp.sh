fca34add05969c439fc1e20a5e5f9c9244bc0986
Brenton Horne <brentonhorne77@gmail.com>
2018-11-07 11:34:39 +1000

    Fixing spup

--- obs/openra/sp.sh
+++ obs/openra/sp.sh
@@ -3,13 +3,13 @@ function spup {
 	splver=$(git log | head -n 1 | cut -d ' ' -f 2)
 	sppver=$(grep 'sp_commit' < /home/fusion809/OBS/home:fusion809/openra-sp/openra-sp.spec | sed 's/%define sp_commit //g')
 	# Latest engine version
-	enlv=$(grep '^ENGINE\_VERSION' < mod.config | cut -d '"' -f 2)
+	enlv=$(grep '^ENGINE\_VERSION' < ../SP-OpenRAModSDK/mod.config | cut -d '"' -f 2)
 	# OpenRA engine version in spec file
 	enpv=$(grep "define engine\_version" < "$OBSH"/openra-sp/openra-sp.spec | cut -d ' ' -f 3)
 	cd ../SP-OpenRAModSDK || exit
 	sdklver=$(git log | head -n 1 | cut -d ' ' -f 2)
 	sdklc=$(git rev-list --branches master --count)
-	sdkpver=$(grep "commit" < cat /home/fusion809/OBS/home:fusion809/openra-sp/openra-sp.spec | sed 's/%define commit //g')
+	sdkpver=$(grep "commit" < /home/fusion809/OBS/home:fusion809/openra-sp/openra-sp.spec | sed 's/%define commit //g')
 	sdkpc=$(grep "Version:" < /home/fusion809/OBS/home:fusion809/openra-sp/openra-sp.spec | sed 's/Version:\s*//g')
 
 	# If OpenRAModSDK is outdated sed update it
@@ -18,7 +18,7 @@ function spup {
 	else
 		 if ! [[ "$enpv" == "$enlv" ]]; then
 			  printf "%s\n" "Updating the game engine to $enlv."
-			  sed -i -e "s/$enpv/$enlv/g" "$OBSH"/openra-sp/{openra-sp.spec,PKGBUILD}
+			  sed -i -e "s/${enpv}/${enlv}/g" "$OBSH"/openra-sp/{openra-sp.spec,PKGBUILD}
 			  make clean || exit
 			  make || exit
 			  tar czvf "$OBSH"/openra-sp/engine-"${enlv}".tar.gz engine
@@ -28,8 +28,8 @@ function spup {
 			  cd - || exit
 		 fi
 		 cdobsh openra-sp || exit
-		 sed -i -e "s|$sdkpc|$sdklc|g" \
-				-e "s|$sdkpver|$sdklver|g" openra-sp.spec
+		 sed -i -e "s|${sdkpc}|${sdklc}|g" \
+				-e "s|${sdkpver}|${sdklver}|g" openra-sp.spec
 	fi
 
 	# If Shattered Paradise is outdated sed update it
@@ -37,7 +37,7 @@ function spup {
 		 printf "%s\n" "Latest Shattered Paradise commit is packaged!"
 	else
 		 cdobsh openra-sp || exit
-		 sed -i -e "s|$sppver|$splver|g" openra-sp.spec
+		 sed -i -e "s|${sppver}|${splver}|g" openra-sp.spec
 	fi
 
 	# If OpenRAModSDK or Shattered Paradise repo is outdated commit updtae to repo
