2efe8b275d855aed6d51ce5f6df5a81a5c56d6c0
Brenton Horne <brentonhorne77@gmail.com>
2018-11-23 17:13:34 +1000

    Adding Guix to PATH

--- variables/09-PATH.sh
+++ variables/09-PATH.sh
@@ -4,35 +4,35 @@ export PATH=/bin:/sbin:/usr/bin:/usr/sbin
 export PATH=$PATH:/usr/local/bin:/usr/local/sbin
 
 if [[ -d $HOME/.local/bin ]]; then
-    export PATH=$HOME/.local/bin:$PATH
+	export PATH=$HOME/.local/bin:$PATH
 fi
 
 if [[ -d $HOME/.bin ]]; then
-    export PATH=$HOME/.bin:$PATH
+	export PATH=$HOME/.bin:$PATH
 fi
 
 if [[ -d /usr/lib/hardening-wrapper/bin ]]; then
-    export PATH=$PATH:/usr/lib/hardening-wrapper/bin
+	export PATH=$PATH:/usr/lib/hardening-wrapper/bin
 fi
 
 # Java
 if [[ -n $JAVA_HOME ]]; then
-    export PATH=$PATH:$JAVA_HOME/bin
+	export PATH=$PATH:$JAVA_HOME/bin
 fi
 
 # Gem
 if [[ -n $GEMPATH ]]; then
-    export PATH=$PATH:$GEMPATH
+	export PATH=$PATH:$GEMPATH
 fi
 
 # Go
 if [[ -n $GOPATH ]]; then
-    export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
+	export PATH=$PATH:$GOPATH/bin:$GOROOT/bin
 fi
 
 # Perl
 if [[ -n $PERL_PATH ]]; then
-    export PATH=$PATH:$PERL_PATH
+	export PATH=$PATH:$PERL_PATH
 fi
 
 # Nix
@@ -43,20 +43,25 @@ fi
 
 # Guix
 if [[ -d $HOME/.guix-profile/bin ]]; then
-    export PATH=$PATH:$HOME/.guix-profile/bin
+	export PATH=$PATH:$HOME/.guix-profile/bin
 fi
 
 if [[ -d $HOME/.guix-profile/sbin ]]; then
-    export PATH=$PATH:$HOME/.guix-profile/sbin
+	export PATH=$PATH:$HOME/.guix-profile/sbin
 fi
 
 # Linuxbrew
 export BREW_PATH=/home/linuxbrew/.linuxbrew
 if [[ -d $BREW_PATH/bin ]]; then
-    export PATH=$PATH:$BREW_PATH/bin:$BREW_PATH/sbin
+	export PATH=$PATH:$BREW_PATH/bin:$BREW_PATH/sbin
 fi
 
 # JChem
 if [[ -d /opt/chemaxon/jchemsuite/bin ]]; then
-    export PATH=$PATH:/opt/chemaxon/jchemsuite/bin
+	export PATH=$PATH:/opt/chemaxon/jchemsuite/bin
+fi
+
+# GNU Guix
+if [[ -d $HOME/.guix-profile ]]; then
+	export PATH=$HOME/.guix-profile/bin:$HOME/.guix-profile/sbin${PATH:+:}$PATH
 fi
