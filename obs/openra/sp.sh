ae2021ee08514dfb0bfe358e2f38573865d58bbf
Brenton Horne <brentonhorne77@gmail.com>
2018-12-03 00:50:16 +1000

    spup: fixing sdkpver so the func works

--- obs/openra/sp.sh
+++ obs/openra/sp.sh
@@ -1,15 +1,16 @@
 function spup {
-	cdgo Shattered-Paradise || exit
+	cdgo Shattered-Paradise || return
+	git pull origin master || return
 	splver=$(git log | head -n 1 | cut -d ' ' -f 2)
-	sppver=$(grep 'sp_commit' < /home/fusion809/OBS/home:fusion809/openra-sp/openra-sp.spec | sed 's/%define sp_commit //g')
+	sppver=$(grep '%define sp_commit' < /home/fusion809/OBS/home:fusion809/openra-sp/openra-sp.spec | sed 's/%define sp_commit //g')
 	# Latest engine version
 	enlv=$(grep '^ENGINE\_VERSION' < ../SP-OpenRAModSDK/mod.config | cut -d '"' -f 2)
 	# OpenRA engine version in spec file
-	enpv=$(grep "define engine\_version" < "$OBSH"/openra-sp/openra-sp.spec | cut -d ' ' -f 3)
-	cd ../SP-OpenRAModSDK || exit
+	enpv=$(grep "%define engine\_version" < "$OBSH"/openra-sp/openra-sp.spec | cut -d ' ' -f 3)
+	cd ../SP-OpenRAModSDK || return
 	sdklver=$(git log | head -n 1 | cut -d ' ' -f 2)
 	sdklc=$(git rev-list --branches master --count)
-	sdkpver=$(grep "commit" < /home/fusion809/OBS/home:fusion809/openra-sp/openra-sp.spec | sed 's/%define commit //g')
+	sdkpver=$(grep "%define commit" < /home/fusion809/OBS/home:fusion809/openra-sp/openra-sp.spec | sed 's/%define commit //g')
 	sdkpc=$(grep "Version:" < /home/fusion809/OBS/home:fusion809/openra-sp/openra-sp.spec | sed 's/Version:\s*//g')
 
 	# If OpenRAModSDK is outdated sed update it
@@ -18,31 +19,31 @@ function spup {
 	else
 		 if ! [[ "$enpv" == "$enlv" ]]; then
 			  printf "%s\n" "Updating the game engine to $enlv."
-			  sed -i -e "s/${enpv}/${enlv}/g" "$OBSH"/openra-sp/{openra-sp.spec,PKGBUILD}
-			  make clean || exit
-			  make || exit
+			  sed -i -e "s|${enpv}|${enlv}|g" "$OBSH"/openra-sp/{openra-sp.spec,PKGBUILD} || ( printf "Replacing enpv ($enpv) with enlv ($enlv) failed" && return) 
+			  make clean || return
+			  make || return
 			  tar czvf "$OBSH"/openra-sp/engine-"${enlv}".tar.gz engine
-			  cdobsh openra-sp || exit
+			  cdobsh openra-sp || return
 			  osc rm engine-"${enpv}".tar.gz
 			  osc add engine-"${enlv}".tar.gz
-			  cd - || exit
+			  cd - || return
 		 fi
-		 cdobsh openra-sp || exit
-		 sed -i -e "s|${sdkpc}|${sdklc}|g" \
-				-e "s|${sdkpver}|${sdklver}|g" openra-sp.spec
+		 cdobsh openra-sp || return
+		 sed -i -e "s|${sdkpc}$|${sdklc}|g" PKGBUILD openra-sp.spec || ( printf "Replacing sdkpc ($sdkpc) with sdklc ($sdklc) failed" && return )
+		 sed -i -e "s|${sdkpver}|${sdklver}|g" PKGBUILD openra-sp.spec || ( printf "Replacing sdkpver ($sdkpver) with sdklver ($sdklver) failed" && return )
 	fi
 
 	# If Shattered Paradise is outdated sed update it
 	if [[ "$splver" == "$sppver" ]]; then
 		 printf "%s\n" "Latest Shattered Paradise commit is packaged!"
 	else
-		 cdobsh openra-sp || exit
-		 sed -i -e "s|${sppver}|${splver}|g" openra-sp.spec
+		 cdobsh openra-sp || return
+		 sed -i -e "s|${sppver}|${splver}|g" openra-sp.spec || (printf "Replacing sppver ($sppver) with splver ($splver) failed" && return)
 	fi
 
 	# If OpenRAModSDK or Shattered Paradise repo is outdated commit updtae to repo
 	if ! [[ "$sdkpver" == "$sdkpver" ]] || ! [[ "$splver" == "$sppver" ]]; then
-		 cdobsh openra-sp || exit
+		 cdobsh openra-sp || return
 		 osc ci -m "Bumping to SDK: $sdklc ($sdklver); SP: $splver"
 	fi
 	mod-build SP-OpenRAModSDK
