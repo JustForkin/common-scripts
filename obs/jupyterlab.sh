c5ff05e27e151ef9f9917752a51f59c5a0375c76
Brenton Horne <brentonhorne77@gmail.com>
2018-12-20 08:50:41 +1000

    Making jupup use /home/fusion809/GitHub/mine/packaging/jupyterlab-archpkg instead of abs/jupyterlab

--- obs/jupyterlab.sh
+++ obs/jupyterlab.sh
@@ -2,20 +2,22 @@ function jupup {
     cdgo jupyterlab
     git pull origin master -q
     mastn=$(comno)
-    pkgn=$(grep "^pkgver=" < /data/abs/jupyterlab/PKGBUILD | cut -d '=' -f 2)
+    pkgn=$(grep "^pkgver=" < /data/GitHub/mine/packaging/jupyterlab-archpkg/PKGBUILD | cut -d '=' -f 2)
     comm=$(git log | head -n 1 | cut -d ' ' -f 2)
-    pkgm=$(grep "^_commit=" < /data/abs/jupyterlab/PKGBUILD | cut -d '=' -f 2)
+    pkgm=$(grep "^_commit=" < /data/GitHub/mine/packaging/jupyterlab-archpkg/PKGBUILD | cut -d '=' -f 2)
 
     if [[ $pkgn == $mastn ]]; then
          printf "JupyterLab is up to date!\n"
     else
+	 cdpk jupyterlab-archpkg
          sed -i -e "s/$pkgn/$mastn/g" \
-		-e "s/$pkgm/$comm/g"  /data/abs/jupyterlab/PKGBUILD
+		-e "s/$pkgm/$comm/g"  PKGBUILD
 	 if command -v updpkgsums > /dev/null 2>&1 ; then
 		 updpkgsums
 		 if grep -i "Arch Linux" < /etc/os-release > /dev/null 2>&1 ; then
 			 makepkg -sifC --noconfirm
 		 fi
+		 push ":arrow_up: $mastn"
 	 fi
     fi
 }
