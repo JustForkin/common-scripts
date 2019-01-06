a4af1f0cf1dcd2094b9067ecfe92dac6832fb1cb
Brenton Horne <brentonhorne77@gmail.com>
2019-01-06 14:49:43 +1000

    nixoup: Adding engine owner and repo update feature

--- obs/openra/build-all.sh
+++ obs/openra/build-all.sh
@@ -17,12 +17,16 @@ function nixoup {
 	# Present commit hash
 	commitc=$(loge)
 	printf '\e[1;32m%-6s\e[m\n' "Current commit hash (commitc) is ${commitc}."
-	# Engine name
-	enginec=$(grep "^ENGINE_VERSION" < mod.config | cut -d '"' -f 2)
-	printf '\e[1;32m%-6s\e[m\n' "Current engine version (enginec) is ${enginec}."
-	if [[ $enginec == "{DEV_VERSION}" ]]; then
-		git -C $GHUBO/OpenRA pull origin bleed
-		enginec=$(git -C $GHUBO/OpenRA log | head -n 1 | cut -d ' ' -f 2)
+	# Engine version
+	if ( grep "^AUTOMATIC_ENGINE_SOURCE" < mod.config | grep -v 'ENGINE_VERSION' &> /dev/null ) && ( grep '^AUTOMATIC_ENGINE_MANAGEMENT="True"' < mod.config &> /dev/null); then
+		enginec=$(grep "^AUTOMATIC_ENGINE_SOURCE" < mod.config | cut -d '.' -f 2 | cut -d '/' -f 5)
+	else
+		enginec=$(grep "^ENGINE_VERSION" < mod.config | cut -d '"' -f 2)
+		printf '\e[1;32m%-6s\e[m\n' "Current engine version (enginec) is ${enginec}."
+		if [[ $enginec == "{DEV_VERSION}" ]]; then
+			git -C $GHUBO/OpenRA pull origin bleed
+			enginec=$(git -C $GHUBO/OpenRA log | head -n 1 | cut -d ' ' -f 2)
+		fi
 	fi
 	# Presently packaged versions
 	enginen=$(grep '^\s*engine-version' < $NIXPKGS/pkgs/games/openra-$MOD/default.nix | cut -d '"' -f 2)
@@ -46,11 +50,15 @@ function nixoup {
 	#printf '\e[1;32m%-6s\e[m\n' "Checksum for mod's latest commit's tar archive (sha256_1) is ${sha256_1}."
 	# Engine
 	# Owner name
-	owner2=$(grep "owner" < $NIXPKGS/pkgs/games/openra-${MOD}/default.nix | head -n 3 | tail -n 1 | cut -d '"' -f 2)
-	printf '\e[1;32m%-6s\e[m\n' "Engine owner's name (owner2) is ${owner2}."
+    owner2c=$(grep "^AUTOMATIC_ENGINE_SOURCE" < mod.config | cut -d '.' -f 2 | cut -d '/' -f 2)
+	printf '\e[1;32m%-6s\e[m\n' "New engine owner's name (owner2c) is ${owner2c}."
+	owner2n=$(grep "owner" < $NIXPKGS/pkgs/games/openra-${MOD}/default.nix | head -n 3 | tail -n 1 | cut -d '"' -f 2)
+	printf '\e[1;32m%-6s\e[m\n' "Packaged engine owner's name (owner2n) is ${owner2n}."
 	# Repo name
-	repo2=$(grep "repo" < $NIXPKGS/pkgs/games/openra-${MOD}/default.nix | head -n 3 | tail -n 1 | cut -d '"' -f 2)
-	printf '\e[1;32m%-6s\e[m\n' "Engine repo's name (repo2) is ${repo2}."
+	repo2c=(grep "^AUTOMATIC_ENGINE_SOURCE" < mod.config | cut -d '.' -f 2 | cut -d '/' -f 3)
+	printf '\e[1;32m%-6s\e[m\n' "New engine repo's name (repo2c) is ${repo2c}."
+	repo2n=$(grep "repo" < $NIXPKGS/pkgs/games/openra-${MOD}/default.nix | head -n 3 | tail -n 1 | cut -d '"' -f 2)
+	printf '\e[1;32m%-6s\e[m\n' "Packaged engine repo's name (repo2n) is ${repo2n}."
 	# Checksum for tar repo2's archive
 	#nix-prefetch-url https://github.com/$owner2/$repo2/archive/${enginec}.tar.gz &> /tmp/sha256_2
 	#sha256_2=$(cat /tmp/sha256_2 | tail -n 1)
@@ -66,12 +74,16 @@ function nixoup {
 		sed -i -e "13s|${numbn}|${numbc}|" \
 		       -e "25s|${commitn}|${commitc}|" \
 		       -e "14s|${enginen}|${enginec}|" \
+			   -e "30s|${owner2n}|${owner2c}|" \
+			   -e "31s|${repo2n}|${repo2c}|" \
 			   -e "26s|sha256 = \".*\"|sha256 = \"1q5jbkpyhz1p0n7w0v8g6l8p3xcbnmcn0hvf3wxxs48n6fjyw6f9\"|" \
 			   -e "33s|sha256 = \".*\"|sha256 = \"1q5jbkpyhz1p0n7w0v8g6l8p3xcbnmcn0hvf3wxxs48n6fjyw6f2\"|" \
 			   $NIXPKGS/pkgs/games/openra-${MOD}/default.nix
 	else
 		sed -i -e "13s|${numbn}|${numbc}|g" \
 		       -e "25s|${commitn}|${commitc}|g" \
+			   -e "30s|${owner2n}|${owner2c}|" \
+			   -e "31s|${repo2n}|${repo2c}|" \
 			   -e "26s|sha256 = \".*\"|sha256 = \"1q5jbkpyhz1p0n7w0v8g6l8p3xcbnmcn0hvf3wxxs48n6fjyw694\"|" \
 			   $NIXPKGS/pkgs/games/openra-${MOD}/default.nix
 	fi
