59127564088c28db6a4846c7519f48ae5b2e57fd
Brenton Horne <brentonhorne77@gmail.com>
2019-01-11 15:51:58 +1000

    Updating msteen's nix-prefetch func

--- obs/openra/01-nixpkgs-update.sh
+++ obs/openra/01-nixpkgs-update.sh
@@ -1,34 +1,49 @@
-function nix-prefetch-src {
-  local help_ret=1 nixpkgs
+# This function is from msteen @ GitHub.
+nix-prefetch() {
+  local help_ret=1 nixpkgs expr
   if
     [[ $1 =~ ^(-h|--help)$ ]] && help_ret=0 || {
-      (( $# > 1 )) && nixpkgs=$1 && shift || nixpkgs='<nixpkgs>'
-      ! (( $# == 1 ))
+      (( $# == 2 )) && nixpkgs=$1 && shift || nixpkgs='<nixpkgs>'
+      (( $# == 1 )) && expr=$1 && shift
+      ! (( $# == 0 ))
     }
   then
     help_msg='Print the the hash of the sources.
 
 Usage:
-  nix-prefetch-src [<nixpkgs>] <srcs>
-  nix-prefetch-src -h | --help
+  nix-prefetch [<nixpkgs>] <srcs>
+  nix-prefetch -h | --help
 
 Options:
   -h, --help  Show help message.'
     ! (( help_ret )) && echo "$help_msg" || echo "$help_msg" >&2
     return $help_ret
   fi
+  local src_drvs src_drv hash_size output
   src_drvs=$(nix eval --raw '(
     let
-      x = with import '"$nixpkgs"' { }; ('"$1"');
+      pkgs = import '"$nixpkgs"' { };
+      x = with pkgs; '"$expr"';
       xs = if builtins.isList x then x else [ x ];
-      srcs = builtins.concatMap (x: if builtins.isList x then x else x.srcs or [ (x.src or x) ]) xs;
-      src_drvs = map (src: (src.overrideAttrs (_: { outputHash = "0000000000000000000000000000000000000000000000000000"; })).drvPath) srcs;
-    in builtins.concatStringsSep "\n" src_drvs
+      srcs = builtins.concatMap (x: x.srcs or [ (x.src or x) ]) xs;
+      dummyHashes = {
+        sha1   = "00000000000000000000000000000000";
+        sha256 = "0000000000000000000000000000000000000000000000000000";
+        sha512 = "0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+      };
+      dummySrcs = map (src: src.overrideAttrs (oldAttrs: {
+        outputHash = dummyHashes.${oldAttrs.outputHashAlgo};
+      })) srcs;
+      srcDrvs = map (src: "${src.drvPath} ${toString (builtins.stringLength src.outputHash)}") dummySrcs;
+    in builtins.concatStringsSep "\n" srcDrvs
   )') &&
-  while IFS= read -r src_drv; do
-    result=$(nix-store --quiet --realize "$src_drv" 2>&1) &&
-    nix-store --query --hash "$result" | cut -d: -f2 ||
-    sed -n 's/.*\([a-z0-9]\{52\}\).*[a-z0-9]\{52\}.*/\1/p' <<< "$result"
+  while IFS=' ' read -r src_drv hash_size; do
+    if output=$(nix-store --quiet --realize "$src_drv" 2>&1); then
+      echo "Something went wrong with the output hash, the dummy hash should always fail to build." >&2
+      return 1
+    else
+      sed -n "s/.*\([a-z0-9]\{$hash_size\}\).*[a-z0-9]\{$hash_size\}.*/\1/p" <<< "$output"
+    fi
   done <<< "$src_drvs"
 }
 
@@ -40,7 +55,6 @@ function verpres {
 	if ( ! [[ -n "$1" ]] ) || [[ "$1" == "1" ]]; then
 		grep "^    version = \"" < $NIXPATH/mods.nix | head -n 1 | cut -d '"' -f 2
 	else
-		tailno=$((${1}-1))
 		grep "^    version = \"" < $NIXPATH/mods.nix | head -n "$1" | tail -n 1 | cut -d '"' -f 2
 	fi
 }
@@ -67,16 +81,20 @@ function comenpres {
 function engnew {
 	# This does assume that if ENGINE_VERSION is {DEV_VERSION} my local copy of the engine repo is on the right branch for the mod
 	# e.g. that OpenRA-mw is on the MedievalWarfareEngine branch
+	# engine version per mod.config
 	engver=$(grep "^ENGINE\_VERSION" < $1/mod.config | cut -d '"' -f 2)
+	# The engine's repo name
 	engsrc=$(grep "ENGINE\_VERSION" < $1/mod.config | tail -n 1 | cut -d '"' -f 2 | cut -d '/' -f 5)
 	MOD_ID=$(grep "^MOD_ID" < $1/mod.config | head -n 1 | cut -d '"' -f 2)
-	# The following two should match
+	# The following two should match, assuming $engsrc is the name of the repo copy in $GHUBO
 	engsrcurl=$(grep "^AUTOMATIC_ENGINE_SOURCE" < $1/mod.config | tail -n 1 | cut -d '"' -f 2 | sed 's|https://||g' | sed 's|/archive/.*.zip||g')
 	if [[ -d $GHUBO/$engsrc ]]; then
 		engsrcrepoorigin=$(git -C $GHUBO/$engsrc remote -v | head -n 1 | sed 's|.*://||g' | sed 's/.git (fetch)//g' | sed 's/ (fetch)//g')
+	elif [[ -d $GHUBO/OpenRA-mods/$engsrc ]]; then
+		engsrcrepoorigin=$(git -C $GHUBO/OpenRA-mods/$engsrc remote -v | head -n 1 | sed 's|.*://||g' | sed 's/.git (fetch)//g' | sed 's/ (fetch)//g')
 	fi
 
-	if ! [[ -d $GHUBO/$engsrc ]]; then
+	if ( ! [[ -d $GHUBO/$engsrc ]] ) || ( ! [[ -d $GHUBO/OpenRA-mods/$engsrc ]] ); then
 		git clone -q https://$engsrcurl $GHUBO/$engsrc
 		engsrcrepoorigin="${engineurl}"
 	fi
@@ -88,7 +106,7 @@ function engnew {
 		elif [[ -d $GHUBO/OpenRA-mods/OpenRA-${MOD_ID} ]]; then
 			engsrc="OpenRA-mods/OpenRA-${MOD_ID}"
 			engsrcrepoorigin=$(git -C $GHUBO/$engsrc remote -v | head -n 1 | sed 's|.*://||g' | sed 's/.git (fetch)//g' | sed 's/ (fetch)//g')
-	    else
+	  else
 			engsrcrepoorigin=$(git -C $GHUBO/$engsrc remote -v | head -n 1 | sed 's|.*://||g' | sed 's/.git (fetch)//g' | sed 's/ (fetch)//g')
 			printf "Cannot find engine source directory, as it doesn't seem to be either $engsrc, nor $GHUBO/OpenRA-${MOD_ID}, nor $GHUBO/OpenRA-mods/OpenRA-${MOD_ID}.\n" && return
 		fi
@@ -128,10 +146,10 @@ function nixoup2 {
 	sed -i -e "${5}s|${engrevpres}|${engrevnew}|" $NIXPATH/mods.nix
 
 	# Check if either engine, or mod has been updated, 
-	# as nix-prefetch-src can chew up a bit of bandwidth unnecessarily if used when there is no need
+	# as nix-prefetch can chew up a bit of bandwidth unnecessarily if used when there is no need
 	if ( ! [[ "$comnew" == "$comprese" ]] ) || ( ! [[ "$engrevnew" == "$engrevpres" ]] ); then
 		MOD_ID=$(grep "^MOD_ID" < $1/mod.config | head -n 1 | cut -d '"' -f 2)
-		sha256=$(nix-prefetch-src $NIXPKGS openraPackages.mods.${MOD_ID})
+		sha256=$(nix-prefetch $NIXPKGS openraPackages.mods.${MOD_ID})
 		# First is the mod's hash, second is engine
 		sha256_1=$(echo $sha256 | head -n 1)
 		sha256_2=$(echo $sha256 | tail -n 1)
@@ -150,17 +168,17 @@ function engine_update {
 	bleed_oldver="$(grep " commit = \"" < $NIXPATH/engines.nix | head -n 1 | cut -d '"' -f 2)"
 	
 	if ! [[ "${release}" == "${release_oldver}" ]] ; then
-		release_sha256=$(nix-prefetch-src $NIXPKGS openraPackages.engines.release)
+		release_sha256=$(nix-prefetch $NIXPKGS openraPackages.engines.release)
 		sed -i -e "25s|\".*\"|\"${release}\"|" \
 	    	   -e "27s|\".*\"|\"${release_sha256}\"|" $NIXPATH/engines.nix
 	fi
 	if ! [[ "${playtest}" == "${playtest_oldver}" ]] ; then
-	    playtest_sha256=$(nix-prefetch-src $NIXPKGS openraPackages.engines.playtest)
+	    playtest_sha256=$(nix-prefetch $NIXPKGS openraPackages.engines.playtest)
 		sed -i -e "31s|\".*\"|\"${playtest}\"|" \
 		   	   -e "33s|\".*\"|\"${playtest_sha256}\"|" $NIXPATH/engines.nix
 	fi
 	if ! [[ "${bleed}" == "${bleed_oldver}" ]] ; then
-		bleed_sha256=$(nix-prefetch-src $NIXPKGS openraPackages.engines.bleed)
+		bleed_sha256=$(nix-prefetch $NIXPKGS openraPackages.engines.bleed)
 		sed -i -e "36s|\".*\"|\"${bleed}\"|" \
 		       -e "39s|\".*\"|\"${bleed_sha256}\"|" $NIXPATH/engines.nix
     fi
