51ba9851de59da8f453c342fbf3a119168bc8abe
Brenton Horne <brentonhorne77@gmail.com>
2019-01-22 01:42:55 +1000

    Moving scripts variables into github file

--- variables/08-github.sh
+++ variables/08-github.sh
@@ -1,21 +1,38 @@
 export GHUB=$HOME/GitHub
-export GHUBM=$GHUB/mine
 export GHUBO=$GHUB/others
+# My GitHub repos
+export GHUBM=$GHUB/mine
 export ATO=$GHUBM/atom
+# - Configs
 export CFG=$GHUBM/config
+export NC=$CFG/NixOS-configs
+# - Editors
 export ED=$GHUBM/editors
+export EDT=$ED
+export VIM=$EDT/vim
+# - Installers (no longer maintained)
 export INS=$GHUBM/installers
-export OTH=$GHUBM/others
+# - Packaging
 export PK=$GHUBM/packaging
+export PKG=$PK
+export NIXPKGS=$PK/nixpkgs
+# - Scripts
 export SCR=$GHUBM/scripts
-export WEB=$GHUBM/websites
-export PKG=$GHUBM/packaging
 export ARS=$SCR/arch-scripts
-export PY=$SCR/python-scripts
+export CS=$SCR/centos-scripts
+export DBS=$SCR/debian-scripts
+export FS=$SCR/fedora-scripts
+export GS=$SCR/gentoo-scripts
 export JS=$SCR/JScripts
 export LFSS=$SCR/lfs-scripts
 export LU=$SCR/lua-scripts
-export EDT=$GHUBM/editors
+export MS=$SCR/mageia-scripts
+export NS=$SCR/nixos-scripts
+export OSS=$SCR/opensuse-scripts
+export PLS=$SCR/pclinuxos-scripts
+export PY=$SCR/python-scripts
+export SS=$SCR/slackware-scripts
+export VS=$SCR/void-scripts
+# - Websites
+export WEB=$GHUBM/websites
 export FGI=$WEB/fusion809.github.io
-export DBS=$SCR/debian-scripts
-export NIXPKGS=$PK/nixpkgs
