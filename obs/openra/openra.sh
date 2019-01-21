e723f50cfbf40d9958b58e77bc3f2a95069a6604
Brenton Horne <brentonhorne77@gmail.com>
2019-01-21 13:02:01 +1000

    +curly braces around vers; using more desc var names

--- obs/openra/openra.sh
+++ obs/openra/openra.sh
@@ -5,12 +5,12 @@ function update_openra_obs_package {
     # release_type is either playtest or release
     release_type=$(curl -sL https://github.com/OpenRA/OpenRA/releases | grep "[a-z]*-.*\.tar\.gz" | head -n 1 | cut -d '/' -f 5 | cut -d '-' -f 1)
 
-    if ! [[ $latest_openra_version == $packaged_openra_version ]]; then
-         sed -i -e "s|$packaged_openra_version|$latest_openra_version|g" \
-                -e "25s/release\|playtest/$release_type/" \
-                -e "92s/release\|playtest/$release_type/" \
-                -e "95s/release\|playtest/$release_type/" openra.spec
-         osc ci -m "Bumping to $latest_openra_version"
+    if ! [[ ${latest_openra_version} == ${packaged_openra_version} ]]; then
+         sed -i -e "s|${packaged_openra_version}|${latest_openra_version}|g" \
+                -e "25s/release\|playtest/${release_type}/" \
+                -e "92s/release\|playtest/${release_type}/" \
+                -e "95s/release\|playtest/${release_type}/" openra.spec
+         osc ci -m "Bumping to ${latest_openra_version}"
     fi
 }
 
