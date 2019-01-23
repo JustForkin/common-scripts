ba9dd7eb054e27878509d6db26aab264b0974676
Brenton Horne <brentonhorne77@gmail.com>
2019-01-23 14:29:56 +1000

    engine_update: adding committing feature; fixing sha256

--- nixpkgs/05-engine-update.sh
+++ nixpkgs/05-engine-update.sh
@@ -7,20 +7,29 @@ function engine_update {
 	old_bleed_version="$(grep " commit = \"" < $OPENRA_NIXPKG_PATH/engines.nix | head -n 1 | cut -d '"' -f 2)"
 	
 	if ! [[ "${latest_release_version}" == "${old_release_version}" ]] ; then
-		release_sha256=$(nix-prefetch $NIXPKGS openraPackages.engines.release)
+		release_sha256=$(nix-prefetch --force $NIXPKGS openraPackages.engines.release)
+		printf "latest_release_version is ${latest_release_version}.\n"
+		printf "release_sha256 is ${release_sha256}.\n"
 		sed -i -e "25s|\".*\"|\"${latest_release_version}\"|" $OPENRA_NIXPKG_PATH/engines.nix || (printf "Sedding release version (${latest_release_version}) failed at line 11 of 06-engine.sh.\n" && return)
 	    sed -i -e "27s|\".*\"|\"${release_sha256}\"|" $OPENRA_NIXPKG_PATH/engines.nix || (printf "Sedding release (${latest_release_version}) hash (${release_sha256}) failed at line 12 of 06-engine.sh.\n" && return)
+		push "openra: :arrow_up: ${latest_release_version}"
 	fi
 
 	if ! [[ "${latest_playtest_version}" == "${old_playtest_version}" ]] ; then
-        playtest_sha256=$(nix-prefetch $NIXPKGS openraPackages.engines.playtest)
+        playtest_sha256=$(nix-prefetch --force $NIXPKGS openraPackages.engines.playtest)
+		printf "latest_playtest_version is ${latest_playtest_version}.\n"
+		printf "playtest_sha256 is ${playtest_sha256}.\n"
 		sed -i -e "31s|\".*\"|\"${latest_playtest_version}\"|" $OPENRA_NIXPKG_PATH/engines.nix || (printf "Sedding playtest version (${latest_playtest_version}) failed at line 17 of 06-engine.sh.\n" && return)
 		sed -i -e "33s|\".*\"|\"${playtest_sha256}\"|" $OPENRA_NIXPKG_PATH/engines.nix || (printf "Sedding playtest (${latest_playtest_version}) hash (${playtest_sha256}) failed at line 18 of 06-engine.sh.\n" && return)
+		push "openra: :arrow_up: ${latest_playtest_version}"
 	fi
 
 	if ! [[ "${latest_bleed_version}" == "${old_bleed_version}" ]] ; then
-		bleed_sha256=$(nix-prefetch $NIXPKGS openraPackages.engines.bleed)
+		bleed_sha256=$(nix-prefetch --force $NIXPKGS openraPackages.engines.bleed)
+		printf "latest_bleed_version is ${latest_bleed_version}.\n"
+		printf "bleed_sha256 is ${bleed_sha256}.\n"
 		sed -i -e "36s|\".*\"|\"${latest_bleed_version}\"|" $OPENRA_NIXPKG_PATH/engines.nix || (printf "Sedding bleed version (${latest_bleed_version}) failed at line 23 of 06-engine.sh.\n" && return)
-		sed -i -e "39s|\".*\"|\"${bleed_sha256}\"|" $OPENRA_NIXPKG_PATH/engines.nix  || (printf "Sedding bleed version (${bleed_sha256}) hash (${bleed_sha256}) failed at line 24 of 06-engine.sh.\n" && return)
-  fi
+		sed -i -e "39s|\".*\"|\"${bleed_sha256}\"|" $OPENRA_NIXPKG_PATH/engines.nix || (printf "Sedding bleed version (${latest_bleed_version}) hash (${bleed_sha256}) failed at line 24 of 06-engine.sh.\n" && return)
+		push "openra: :arrow_up: ${latest_bleed_version}"
+	fi
 }
