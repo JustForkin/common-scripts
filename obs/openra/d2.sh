c62c68e2b571028e0d146a6ea6f1824aff563361
Brenton Horne <brentonhorne77@gmail.com>
2019-02-01 03:14:43 +1000

    Adjusting colouring

--- obs/openra/d2.sh
+++ obs/openra/d2.sh
@@ -10,15 +10,15 @@ function d2up {
     # OpenRA engine version in spec file
     packaged_engine_version=$(grep "define engine\_version" < "$HOME"/OBS/home:fusion809/openra-d2/openra-d2.spec | cut -d ' ' -f 3)
 
-    if [[ $packaged_commit_number == $latest_commit_no ]]; then
-         printf "%s\n" "OpenRA RA2 is up-to-date!"
+    if [[ ${packaged_commit_number} == ${latest_commit_no} ]]; then
+         printf "\e[1;32m%-0s\e[m\n" "OpenRA RA2 is up-to-date!"
     else
-         printf "%s\n" "Updating d2 spec file and PKGBUILD."
-         sed -i -e "s/$packaged_commit_hash/$latest_commit_hash/g" \
-			 -e "s/$packaged_commit_number/$latest_commit_no/g" "$OBSH"/openra-d2/{openra-d2.spec,PKGBUILD}
+         printf "\e[1;34m%-0s\e[m\n" "Updating d2 spec file and PKGBUILD."
+         sed -i -e "s/${packaged_commit_hash}/${latest_commit_hash}/g" \
+			 -e "s/${packaged_commit_number}/${latest_commit_no}/g" "$OBSH"/openra-d2/{openra-d2.spec,PKGBUILD}
          if ! [[ "$packaged_engine_version" == "$latest_engine_version" ]]; then
-              printf "%s\n" "Updating engine to $latest_engine_version."
-              sed -i -e "s/define engine_version $packaged_engine_version/define engine_version $latest_engine_version/g" "$HOME"/OBS/home:fusion809/openra-d2/{openra-d2.spec,PKGBUILD}
+              printf "\e[1;34m%-0s\e[m\n" "Updating engine to $latest_engine_version."
+              sed -i -e "s/define engine_version ${packaged_engine_version}/define engine_version ${latest_engine_version}/g" "$HOME"/OBS/home:fusion809/openra-d2/{openra-d2.spec,PKGBUILD}
               make clean || return
               make || return
               tar czvf "$HOME/OBS/home:fusion809/openra-d2/engine-${latest_engine_version}.tar.gz" engine
@@ -27,9 +27,9 @@ function d2up {
               osc add engine-"${latest_engine_version}".tar.gz
               cd - || return
          fi
-         printf "%s\n" "Committing changes."
+         printf "\e[1;34m%-0s\e[m\n" "Committing changes."
          cdobsh openra-d2 || return
-         osc ci -m "Bumping $packaged_commit_number->$latest_commit_no"
+         osc ci -m "Bumping ${packaged_commit_number}->${latest_commit_no}"
     fi
 
     	d2nup "${1}"
--- obs/openra/gen.sh
+++ obs/openra/gen.sh
@@ -11,15 +11,15 @@ function genup {
 	latest_commit_hash=$(latest_commit_on_branch)
 	packaged_commit_hash=$(come openra-gen)
 
-	if [[ $packaged_commit_number == $latest_commit_no ]]; then
-		printf "%s\n" "OpenRA Generals Alpha is up-to-date\!"
+	if [[ ${packaged_commit_number} == ${latest_commit_no} ]]; then
+		printf "\e[1;32m%-0s\e[m\n" "OpenRA Generals Alpha is up-to-date\!"
 	else
-		printf "%s\n" "Updating openra-gen spec file and PKGBUILD."
-		sed -i -e "s/$packaged_commit_hash/$latest_commit_hash/g" \
-			   -e "s/$packaged_commit_number/$latest_commit_no/g" "$OBSH"/openra-gen/{openra-gen.spec,PKGBUILD}
-		if ! [[ $packaged_engine_version == $latest_engine_version ]]; then
-			printf "%s\n" "Updating Generals Alpha engine."
-			sed -i -e "s/$packaged_engine_version/$latest_engine_version/g" "$HOME"/OBS/home:fusion809/openra-gen/{openra-gen.spec,PKGBUILD}
+		printf "\e[1;34m%-0s\e[m\n" "Updating openra-gen spec file and PKGBUILD."
+		sed -i -e "s/${packaged_commit_hash}/${latest_commit_hash}/g" \
+			   -e "s/${packaged_commit_number}/${latest_commit_no}/g" "$OBSH"/openra-gen/{openra-gen.spec,PKGBUILD}
+		if ! [[ ${packaged_engine_version} == ${latest_engine_version} ]]; then
+			printf "\e[1;34m%-0s\e[m\n" "Updating Generals Alpha engine."
+			sed -i -e "s/${packaged_engine_version}/${latest_engine_version}/g" "$HOME"/OBS/home:fusion809/openra-gen/{openra-gen.spec,PKGBUILD}
 			make clean || return
 			make || ( printf "Running make failed" && return )
 			tar czvf "$HOME"/OBS/home:fusion809/openra-gen/engine-"${latest_engine_version}".tar.gz generals-alpha-engine
@@ -30,7 +30,7 @@ function genup {
 		fi
 		printf "%s\n" "Committing changes."
 		cdobsh openra-gen || return
-		osc ci -m "Bumping $packaged_commit_number->$latest_commit_no"
+		osc ci -m "Bumping ${packaged_commit_number}->${latest_commit_no}"
 	fi
 	openra_mod_appimage_build Generals-Alpha
 	gennup "${1}"
