6431f71a60b5a621ffac156f8e26529494761d79
Brenton Horne <brentonhorne77@gmail.com>
2019-02-10 04:03:45 +1000

    engine_update: Adjusting bleed version string

--- nixpkgs/05-engine-update.sh
+++ nixpkgs/05-engine-update.sh
@@ -10,29 +10,30 @@ function engine_update {
 	
 	if ! [[ "${latest_release_version}" == "${old_release_version}" ]] ; then
 		printf "latest_release_version is ${latest_release_version}.\n"
-		sed -i -e "25s|\".*\"|\"${latest_release_version}\"|" $OPENRA_NIXPKG_PATH/engines.nix || (printf "Sedding release version (${latest_release_version}) failed at line 11 of 06-engine.sh.\n" && return)
+		sed -i -e "25s|\".*\"|\"${latest_release_version}\"|" $OPENRA_NIXPKG_PATH/engines.nix || (printf "Sedding release version (${latest_release_version}) failed at line 13 of 05-engine-update.sh.\n" && return)
 	    release_sha256=$(nix-prefetch --force $NIXPKGS openraPackages.engines.release)
 		printf "release_sha256 is ${release_sha256}.\n"
-	    sed -i -e "27s|\".*\"|\"${release_sha256}\"|" $OPENRA_NIXPKG_PATH/engines.nix || (printf "Sedding release (${latest_release_version}) hash (${release_sha256}) failed at line 12 of 06-engine.sh.\n" && return)
+	    sed -i -e "27s|\".*\"|\"${release_sha256}\"|" $OPENRA_NIXPKG_PATH/engines.nix || (printf "Sedding release (${latest_release_version}) hash (${release_sha256}) failed at line 16 of 05-engine-update.sh.\n" && return)
 		cdnp; push "openra: :arrow_up: ${latest_release_version}"
 	fi
 
 	if ! [[ "${latest_playtest_version}" == "${old_playtest_version}" ]] ; then
 		printf "latest_playtest_version is ${latest_playtest_version}.\n"
-		sed -i -e "31s|\".*\"|\"${latest_playtest_version}\"|" $OPENRA_NIXPKG_PATH/engines.nix || (printf "Sedding playtest version (${latest_playtest_version}) failed at line 17 of 06-engine.sh.\n" && return)
+		sed -i -e "31s|\".*\"|\"${latest_playtest_version}\"|" $OPENRA_NIXPKG_PATH/engines.nix || (printf "Sedding playtest version (${latest_playtest_version}) failed at line 22 of 05-engine-update.sh.\n" && return)
 		playtest_sha256=$(nix-prefetch --force $NIXPKGS openraPackages.engines.playtest)
 		printf "playtest_sha256 is ${playtest_sha256}.\n"
-		sed -i -e "33s|\".*\"|\"${playtest_sha256}\"|" $OPENRA_NIXPKG_PATH/engines.nix || (printf "Sedding playtest (${latest_playtest_version}) hash (${playtest_sha256}) failed at line 18 of 06-engine.sh.\n" && return)
+		sed -i -e "33s|\".*\"|\"${playtest_sha256}\"|" $OPENRA_NIXPKG_PATH/engines.nix || (printf "Sedding playtest (${latest_playtest_version}) hash (${playtest_sha256}) failed at line 25 of 05-engine-update.sh.\n" && return)
 		cdnp; push "openra: :arrow_up: ${latest_playtest_version}"
 	fi
 
 	if ! [[ "${latest_bleed_hash}" == "${old_bleed_hash}" ]] ; then
 		printf "latest_bleed_hash is ${latest_bleed_hash}.\n"
 		printf "latest_bleed_number is ${latest_bleed_number}.\n"
-		sed -i -e "36s|\".*\"|\"${latest_bleed_hash}\"|" $OPENRA_NIXPKG_PATH/engines.nix || (printf "Sedding bleed version (${latest_bleed_hash}) failed at line 23 of 06-engine.sh.\n" && return)
+		sed -i -e "36s|\".*\"|\"${latest_bleed_hash}\"|" $OPENRA_NIXPKG_PATH/engines.nix || (printf "Sedding bleed hash (${latest_bleed_hash}) failed at line 32 of 05-engine-update.sh.\n" && return)
 		bleed_sha256=$(nix-prefetch --force $NIXPKGS openraPackages.engines.bleed)
 		printf "bleed_sha256 is ${bleed_sha256}.\n"
-		sed -i -e "39s|\".*\"|\"${bleed_sha256}\"|" $OPENRA_NIXPKG_PATH/engines.nix || (printf "Sedding bleed version (${latest_bleed_hash}) hash (${bleed_sha256}) failed at line 24 of 06-engine.sh.\n" && return)
+		sed -i -e "37s|\".*\"|\"${latest_bleed_number}\".git.\${latest_bleed_7_character_hash}\"|" $OPENRA_NIXPKG_PATH/engines.nix || ( printf "Sedding bleed commit version (${latest_bleed_number}.git.${latest_bleed_7_character_hash}) failed at line 35 of 05-engine-update.sh.\n" && return )
+		sed -i -e "39s|\".*\"|\"${bleed_sha256}\"|" $OPENRA_NIXPKG_PATH/engines.nix || (printf "Sedding bleed version (${latest_bleed_hash}) checksum (${bleed_sha256}) failed at line 36 of 05-engine-update.sh.\n" && return)
 		push "openra: :arrow_up: ${latest_bleed_number} (${latest_bleed_7_character_hash})"
 	fi
 }
