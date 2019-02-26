9142e6407c9cb14f11293ba2fdcb24e19ff86429
Brenton Horne <brentonhorne77@gmail.com>
2019-02-26 20:07:24 +1000

    Expanding cdp funcs

--- cd/home/programs.sh
+++ cd/home/programs.sh
@@ -32,6 +32,29 @@ function cdpr {
     cdp rpm/$1
 }
 
+function cdpt {
+	cdp targz/$1
+}
+
+alias cdpg=cdpt
+
+function cdp2 {
+	cdp tarbz2/$1
+}
+
+alias cdpz2=cdp2
+alias cdpbz2=cdp2
+alias cdpt2=cdp2
+alias cdptz2=cdp2
+alias cdptbz2=cdp2
+alias cdpb2=cdp2
+
+function cdpxz {
+	cdp tarxz/$1
+}
+
+alias cdpx=cdpxz
+
 function cdpz {
     cdp zip/$1
 }
